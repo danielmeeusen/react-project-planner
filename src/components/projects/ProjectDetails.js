5485df4c55de000e3c7284cfb62ed448cad6511f
Daniel Meeusen <danmeeusen@gmail.com>
2018-09-22 20:49:56 -0700

    test

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ProjectDetails = props => {
+  // console.log(props);
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title</span>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Exercitationem tempora iure doloribus, dolorum perferendis maxime
+            sequi esse delectus laudantium laboriosam ut molestias adipisci
+            optio dolor aperiam tempore obcaecati nobis accusantium.
+          </p>
+        </div>
+        <div className="card-action grey lighten-4 grey-text">
+          <div>Posted by User</div>
+          <div>2nd September, 2am</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectDetails;
